26fb90f00950fbdb0dcf608385dce770c3590eeb
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 14:50:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const rootURL = 'https://api.github.com'
 function getRepositories() {
   const name = document.getElementById('username').value
   const uri = rootURL + '/users/' + name + '/repos'
+  const xhr = new XMLHttpRequest()
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
