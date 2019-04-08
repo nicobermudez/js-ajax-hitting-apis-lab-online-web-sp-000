5b93d62457fe39305e5ac03b35df5668fa92dbad
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 14:56:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ function getCommits(el) {
   const repoName = el.dataset.repository
   const uri = rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/commits'
   const xhr = new XMLHttpRequest()
-  xhr.addEventListener('load', displayRepositories);
+  xhr.addEventListener('load', displayCommits);
   xhr.open('GET', 'uri');
   xhr.send();
   return false
