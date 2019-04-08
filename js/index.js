8b7e05864460c8d70ec733e5774cfb968c5a9fb4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 15:00:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -56,3 +56,20 @@ function displayCommits() {
   .join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList
 }
+
+function getBranches(el) {
+  const repoName = el.dataset.repository;
+  const uri =
+    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/branches';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayBranches);
+  xhr.open('GET', uri);
+  xhr.send();
+}
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+    .map(branch => '<li>' + branch.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
+
