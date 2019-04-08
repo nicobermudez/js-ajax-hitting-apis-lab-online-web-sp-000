c6b62e0fdd3bc4eac66cd2ffe0ab3bb8300279ad
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 14:53:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function getRepositories() {
   const name = document.getElementById('username').value
   const uri = rootURL + '/users/' + name + '/repos'
   const xhr = new XMLHttpRequest()
-  xhr.addEventListener('load', showRepositories);
+  xhr.addEventListener('load', displayRepositories);
   xhr.open('GET', 'uri');
   xhr.send();
   return false
